d58503d769f22594b07c421c169ea950bc78f3f6
Satyam-sagar2630 <99909799+Satyam-sagar2630@users.noreply.github.com>
2022-02-21 01:13:04 +0530

    Add files via upload

---
+++ min max array/min max array/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+namespace app
+{
+    class Program
+    {
+        static void Main(string[]arg)
+        {
+            int[] num = { 1, 2, 3, 4, 5,};
+            Console.Write("Max number is : ");
+            Console.WriteLine( num.Max());
+            Console.Write("Min number is : ");
+            Console.WriteLine(num.Min());
+            Console.Write("Sum of Array is : ");
+            Console.WriteLine(num.Sum());
+        }
+    }
+}
